de57f5dd717ba98e25297ed433ee22777aacb5f2
Jon Hill <jondhill1984@gmail.com>
2020-11-12 11:36:48 +0200

    Add NYT API implementation

--- components/newsDisplay/newsDisplay.js
+++ components/newsDisplay/newsDisplay.js
@@ -1,8 +1,10 @@
 import styles from "./newsDisplay.module.css";
 
-function SingleArticle({ newsNews, guardianNews }) {
+function SingleArticle({ newsNews, guardianNews, nytNews }) {
   const { container } = styles;
-  let sortedNews = [...newsNews, ...guardianNews];
+  let sortedNews = [...newsNews, ...guardianNews, ...nytNews].sort(function () {
+    return 0.5 - Math.random();
+  });
   return (
     <>
       {sortedNews.map((article) => (
@@ -11,11 +13,34 @@ function SingleArticle({ newsNews, guardianNews }) {
             article.publishedAt
               ? article.publishedAt
               : article.webPublicationDate
+              ? article.webPublicationDate
+              : article.pub_date
           }
         >
-          <div>{article.title ? article.title : article.webTitle}</div>
-          <div>{article.source ? article.source.name : "Guardian"}</div>
-          <a href={article.url ? article.url : article.webUrl}>
+          <div>
+            {article.title
+              ? article.title
+              : article.webTitle
+              ? article.webTitle
+              : article.headline.main}
+          </div>
+          <div>
+            {article.source === "The New York Times"
+              ? "The New York Times"
+              : article.source
+              ? article.source.name
+              : "Guardian"}
+          </div>
+          <a
+            target="_blank"
+            href={
+              article.url
+                ? article.url
+                : article.webUrl
+                ? article.webUrl
+                : article.web_url
+            }
+          >
             Read full story here
           </a>
         </div>
@@ -24,12 +49,17 @@ function SingleArticle({ newsNews, guardianNews }) {
   );
 }
 
-export default function NewsDisplay({ newsNews, guardianNews }) {
+export default function NewsDisplay({ newsNews, guardianNews, nytNews }) {
+  console.log(nytNews);
   const { container } = styles;
   return (
     <>
       <div className={container}>
-        <SingleArticle newsNews={newsNews} guardianNews={guardianNews} />
+        <SingleArticle
+          newsNews={newsNews}
+          guardianNews={guardianNews}
+          nytNews={nytNews}
+        />
       </div>
     </>
   );
--- pages/news.js
+++ pages/news.js
@@ -1,12 +1,13 @@
 import Layout from "../components/layout/layout";
 import NewsDisplay from "../components/newsDisplay/newsDisplay";
 
-newsPage.getInitialProps = async function (context) {
+export async function getServerSideProps(context) {
   let queryString = "covid";
   const apiKey = process.env.NEWS_API_KEY;
   const GApiKey = process.env.GUARDIAN_NEWS_API_KEY;
+  const NytApiKey = process.env.NYT_NEWS_API_KEY;
 
-  const [newsNews, guardianNews] = await Promise.all([
+  const [newsNews, guardianNews, nytNews] = await Promise.all([
     fetch(`https://newsapi.org/v2/everything?q=${queryString}&apiKey=${apiKey}`)
       .then((r) => r.json())
       .then((res) => res.articles),
@@ -15,16 +16,32 @@ newsPage.getInitialProps = async function (context) {
     )
       .then((r) => r.json())
       .then((res) => res.response.results),
+    fetch(
+      `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${queryString}&api-key=${NytApiKey}`
+    )
+      .then((r) => r.json())
+      // .then((res) => console.log(res)),
+      .then((res) => res.response.docs),
   ]);
 
-  return { newsNews, guardianNews };
-};
+  return {
+    props: {
+      newsNews,
+      guardianNews,
+      nytNews,
+    },
+  };
+}
 
-export default function newsPage({ newsNews, guardianNews }) {
+export default function newsPage({ newsNews, guardianNews, nytNews }) {
   return (
     <>
       <Layout>
-        <NewsDisplay newsNews={newsNews} guardianNews={guardianNews} />
+        <NewsDisplay
+          newsNews={newsNews}
+          guardianNews={guardianNews}
+          nytNews={nytNews}
+        />
       </Layout>
     </>
   );
