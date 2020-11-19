78a233a1bcf430dbfaa16da7bc53cdba2bbc2d75
Jon Hill <jondhill1984@gmail.com>
2020-11-19 16:39:27 +0200

    First push of new branch to save work done

--- components/newsSelector/newsSelector.js
+++ components/newsSelector/newsSelector.js
@@ -43,7 +43,7 @@ export default function NewsSelector() {
     if (option === 1) {
       await Promise.all([
         fetch(
-          `https://newsapi.org/v2/top-headlines?country=us&category=${categoryTitle}&apiKey=${apiKey}`
+          `https://newsapi.org/v2/top-headlines?country=us&category=${categoryTitle}&pageSize=15&apiKey=${apiKey}`
         ),
         fetch(
           `http://content.guardianapis.com/${
@@ -52,7 +52,7 @@ export default function NewsSelector() {
               : categoryTitle === "health"
               ? "lifeandstyle/health-and-wellbeing"
               : categoryTitle
-          }?api-key=${GApiKey}`
+          }?&page-size=6&api-key=${GApiKey}`
         ),
         fetch(
           `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:(%22${categoryTitle}%22)&api-key=${NytApiKey}`
@@ -68,27 +68,32 @@ export default function NewsSelector() {
         .then((data) => {
           const store = [];
           data.map((newsArticle) => {
+            // newsArticle.response
+            //   ? console.log(newsArticle.response)
+            //   : console.log("nope");
             if (newsArticle.articles) {
               store.push(newsArticle.articles);
-            } else if (newsArticle.response.results) {
+            } else if (newsArticle.response) {
               store.push(newsArticle.response.results);
-            } else if (newsArticle.response.docs) {
               store.push(newsArticle.response.docs);
             }
+            // else if (newsArticle.response.docs) {
+            //   store.push(newsArticle.response.docs);
+            // }
           });
-          const finalArray = store.flat().sort(() => {
-            return 0.5 - Math.random();
-          });
-          setData(finalArray);
+          // const finalArray = store.flat().sort(() => {
+          //   return 0.5 - Math.random();
+          // });
+          // setData(finalArray);
           setData(store.flat());
         });
     } else {
       await Promise.all([
         fetch(
-          `https://newsapi.org/v2/everything?q=${searchTerm}&language=en&apiKey=${apiKey}`
+          `https://newsapi.org/v2/everything?q=${searchTerm}&language=en&pageSize=15&apiKey=${apiKey}`
         ),
         fetch(
-          `https://content.guardianapis.com/search?q=${searchTerm}&api-key=${GApiKey}`
+          `https://content.guardianapis.com/search?q=${searchTerm}&page-size=6&api-key=${GApiKey}`
         ),
         fetch(
           `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=${NytApiKey}`
--- components/singleArticle/singleArticle.js
+++ components/singleArticle/singleArticle.js
@@ -3,7 +3,9 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function SingleArticle({ data }) {
   const {
-    container,
+    containerTypeOne,
+    article,
+    articleImage,
     articleContainer,
     articleTitle,
     articleItem,
@@ -13,51 +15,39 @@ export default function SingleArticle({ data }) {
 
   // if the data contains an identifier to be a specific source
   // render a specifc type of div
-
   // news API STORIES CAN be of type one, two, 3 and four
-
   // guardian and NTY can only be type 5
-
   // the order is one, two , three, 5, 5, three, 4, 5, 5, 5,
 
+  let newsStoreOne = data.slice(0, 4);
+  let newsStoreTwo = data.slice(5, 9);
+  let newsStoreThree = data.slice(10, 14);
+
+  let gAndNytStoreMain = data.slice(15).sort(() => {
+    return 0.5 - Math.random();
+  });
+
+  const gAndNytStoreOne = gAndNytStoreMain.slice(0, 3);
+  const gAndNytStoreTwo = gAndNytStoreMain.slice(4, 7);
+  const gAndNytStorethree = gAndNytStoreMain.slice(8, 11);
+
+  // console.log(newsStoreOne);
   return (
     <>
-      <div className={container}>
-        {data &&
-          data.map((article) => (
-            <div key={uuidv4()} className={articleContainer}>
+      <div className={containerTypeOne}>
+        {newsStoreOne &&
+          newsStoreOne.map((article) => (
+            <div key={uuidv4()} className={article}>
+              <div className={articleImage}>
+                <img src={article.urlToImage}></img>
+              </div>
+
               <div className={articleTitle}>
-                {article.title
-                  ? article.title.split(" - ")[0].substr(0, 100)
-                  : article.webTitle
-                  ? article.webTitle.split(" - ")[0].substr(0, 100)
-                  : article.headline.main.split(" - ")[0].substr(0, 100)}
+                {article.title.split(" - ")[0].substr(0, 100)}
               </div>
-              <div className={`${articleItem} ${source}`}>
-                {article.source === "The New York Times"
-                  ? "The New York Times"
-                  : article.source
-                  ? article.source.name
-                  : article.source === "Theguardian.comtechnology"
-                  ? "The Guardian"
-                  : "The Guardian"}
-              </div>
-              {article.urlToImage && (
-                <div>
-                  <img src={article.urlToImage} />
-                </div>
-              )}
+              <div className={`${articleItem} ${source}`}>{article.source}</div>
               <div className={`${articleItem} ${link}`}>
-                <a
-                  target="_blank"
-                  href={
-                    article.url
-                      ? article.url
-                      : article.webUrl
-                      ? article.webUrl
-                      : article.web_url
-                  }
-                >
+                <a target="_blank" href={article.url}>
                   Read full story here
                 </a>
               </div>
@@ -67,3 +57,52 @@ export default function SingleArticle({ data }) {
     </>
   );
 }
+
+//     {data &&
+//       data.map((article) => (
+//         // {
+//         //   article.title ? console.count(article) : "nope";
+//         // }
+
+//         <div key={uuidv4()} className={articleContainer}>
+//           <div className={articleTitle}>
+//             {article.title
+//               ? article.title.split(" - ")[0].substr(0, 100)
+//               : article.webTitle
+//               ? article.webTitle.split(" - ")[0].substr(0, 100)
+//               : article.headline.main
+//               ? article.headline.main.split(" - ")[0].substr(0, 100)
+//               : ""}
+//           </div>
+//           <div className={`${articleItem} ${source}`}>
+//             {article.source === "The New York Times"
+//               ? "The New York Times"
+//               : article.source
+//               ? article.source.name
+//               : article.source === "Theguardian.comtechnology"
+//               ? "The Guardian"
+//               : "The Guardian"}
+//           </div>
+//           {article.urlToImage && (
+//             <div>
+//               <img src={article.urlToImage} />
+//             </div>
+//           )}
+//           <div className={`${articleItem} ${link}`}>
+//             <a
+//               target="_blank"
+//               href={
+//                 article.url
+//                   ? article.url
+//                   : article.webUrl
+//                   ? article.webUrl
+//                   : article.web_url
+//               }
+//             >
+//               Read full story here
+//             </a>
+//           </div>
+//         </div>
+//       ))}
+
+// </>
