baa11b715523f84d9bb79ec1e8cab71f783423a5
Jon Hill <jondhill1984@gmail.com>
2020-11-23 16:42:03 +0200

    Update nav styles

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,22 +1,66 @@
-import styles from './nav.module.css'
-import Link from 'next/link'
+import styles from "./nav.module.css";
+import Link from "next/link";
 
 export default function Nav() {
+  const {
+    navWrapper,
+    navContentContainer,
+    appTitle,
+    news,
+    aggegator,
+    appDescription,
+    categoryPagesContainer,
+  } = styles;
   return (
-      <>
-      <div className={styles.navContainer}>
+    <>
+      <div className={navWrapper}>
+        <div className={navContentContainer}>
+          <div className={appTitle}>
+            News <br />
+            Agregator
+          </div>
+          <div className={appDescription}>
+            All the best stories from the best news outlets
+          </div>
+          <div className={categoryPagesContainer}>
             <div>
-              <Link href="/news"><a>News</a></Link>
+              <Link href="/">
+                <a>HeadLines</a>
+              </Link>
             </div>
             <div>
-              <Link href="/books"><a>Books</a></Link>
+              <Link href="/technology">
+                <a>Technology</a>
+              </Link>
             </div>
             <div>
-              <Link href="/"><a>Home</a></Link>
+              <Link href="/science">
+                <a>Science</a>
+              </Link>
             </div>
+            <div>
+              <Link href="/entertainment">
+                <a>Entertainment</a>
+              </Link>
+            </div>
+            <div>
+              <Link href="/business">
+                <a>Business</a>
+              </Link>
+            </div>
+            <div>
+              <Link href="/sport">
+                <a>Sport</a>
+              </Link>
+            </div>
+            <div>
+              <Link href="/health">
+                <a>Health</a>
+              </Link>
+            </div>
+          </div>
         </div>
-      </>
-
-
-  )
+      </div>
+    </>
+  );
 }
--- pages/news.js
+++ pages/news.js
@@ -1,7 +1,7 @@
 import Layout from "../components/layout/layout";
 import NewsSelector from "../components/newsSelector/newsSelector";
 
-export default function newsPage() {
+export default function NewsPage() {
   return (
     <>
       <Layout>
