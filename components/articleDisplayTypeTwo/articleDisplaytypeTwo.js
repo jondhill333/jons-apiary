0547d277b44b4b59fa69017d821243b3366d7a0a
Jon Hill <jondhill1984@gmail.com>
2020-11-27 14:36:59 +0200

    Add all componenets for first display area

---
+++ components/articleDisplayTypeTwo/articleDisplaytypeTwo.js
@@ -0,0 +1,36 @@
+import styles from "./articleDisplayTypeTwo.module.css";
+import { v4 as uuidv4 } from "uuid";
+
+export default function ArticleDisplaytypeTwo({ newsArticle }) {
+  const article = newsArticle[0];
+  const {
+    wrapper,
+    container,
+    image,
+    sourceAndTitle,
+    title,
+    source,
+    link,
+  } = styles;
+
+  return (
+    <div className={wrapper}>
+      <div key={uuidv4()} className={container}>
+        <div className={sourceAndTitle}>
+          <span className={source}>{article.source.name} / </span>
+          <span className={title}>
+            {article.title.split(" - ")[0].substr(0, 100)}
+          </span>
+        </div>
+        <div className={image}>
+          <img src={article.urlToImage} />
+        </div>
+        <div className={link}>
+          <a target="_blank" href={article.url}>
+            Read full story here
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/newsApiArticles/newsApiArticles.js
+++ components/newsApiArticles/newsApiArticles.js
@@ -1,33 +1,28 @@
 import styles from "./newsApiArticles.module.css";
 import { v4 as uuidv4 } from "uuid";
 import ArticleDisplaytypeOne from "../articleDisplayTypeOne/articleDisplaytypeOne";
+import ArticleDisplaytypeTwo from "../articleDisplayTypeTwo/articleDisplaytypeTwo";
+import ArticleDisplaytypeThree from "../articleDisplayTypeThree/articleDisplaytypeThree";
+import ArticleDisplaytypeFour from "../articleDisplayTypeFour/articleDisplaytypeFour";
+import ArticleDisplaytypeFive from "../articleDisplaytypeFive/articleDisplaytypeFive";
 
 export default function NewsApiArticles({ data }) {
-  const {
-    grid,
-    // container,
-    // articleWrapper,
-    // articleContainer,
-    // articleImage,
-    // articleTitle,
-    // source,
-    // link,
-  } = styles;
+  const { grid } = styles;
 
   let articleOne = data.splice(0, 1);
   let articleTwo = data.splice(1, 1);
   let articleThree = data.splice(2, 1);
-  let articleFour = data.splice(3, 1);
-  let articleFive = data.splice(4, 1);
+  let articleFour = data.splice(1, 1);
+  let articleFive = data.splice(0, 1);
 
   return (
     <>
       <div className={grid}>
         <ArticleDisplaytypeOne newsArticle={articleOne} />
-        {/* <ArticleDisplayTypeTwo article={articleTwo}/>
-    <ArticleDisplayTypeThree article={articleThree}/>
-    <ArticleDisplayTypeFour article={articleFour}/>
-    <ArticleDisplayTypeFive article={articleFive}/> */}
+        <ArticleDisplaytypeTwo newsArticle={articleTwo} />
+        <ArticleDisplaytypeThree newsArticle={articleThree} />
+        <ArticleDisplaytypeFour newsArticle={articleFour} />
+        <ArticleDisplaytypeFive newsArticle={articleFive} />
       </div>
     </>
   );
