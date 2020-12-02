c9b2b56df2987591b5d229ef01f74e2c04493990
Jon Hill <jondhill1984@gmail.com>
2020-12-02 15:58:59 +0200

    Update button and menu functionality

--- components/mobileViewMenuButton/mobileViewMenuButton.js
+++ components/mobileViewMenuButton/mobileViewMenuButton.js
@@ -1,10 +1,84 @@
+import Link from "next/link";
+import { useState } from "react";
 import styles from "./mobileViewMenuButton.module.css";
 
 export default function MenuButton() {
-  const { button } = styles;
+  const [display, setDisplay] = useState(false);
+  const {
+    container,
+    mobileMenuNav,
+    link,
+    button,
+    menuIcon,
+    line,
+    menuOpen,
+    menuCloseIcon,
+    closeIcon,
+  } = styles;
+
+  function toggleMenu() {
+    setDisplay(!display);
+  }
   return (
     <>
-      <div className={button}></div>
+      <div className={container}>
+        {display && (
+          <div className={mobileMenuNav}>
+            <div className={link}>
+              <Link href="/">
+                <a>Headlines</a>
+              </Link>
+            </div>
+            <div className={link}>
+              <Link href="/technology">
+                <a>Technology</a>
+              </Link>
+            </div>
+            <div className={link}>
+              <Link href="/science">
+                <a>Science</a>
+              </Link>
+            </div>
+            <div className={link}>
+              <Link href="/culture">
+                <a>Culture</a>
+              </Link>
+            </div>
+            <div className={link}>
+              <Link href="/business">
+                <a>Business</a>
+              </Link>
+            </div>
+            <div className={link}>
+              <Link href="/sport">
+                <a>Sport</a>
+              </Link>
+            </div>
+            <div className={link}>
+              <Link href="/health">
+                <a>Health</a>
+              </Link>
+            </div>
+            <div className={link}></div>
+          </div>
+        )}
+
+        <div className={button} onClick={toggleMenu}>
+          {!display ? (
+            <div className={menuIcon}>
+              <div className={`${line} ${menuOpen}`}></div>
+              <div className={`${line} ${menuOpen}`}></div>
+              <div className={`${line} ${menuOpen}`}></div>
+            </div>
+          ) : (
+            <div className={menuCloseIcon}>
+              <div className={closeIcon}>
+                <img src="/cancel.png"></img>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
 }
