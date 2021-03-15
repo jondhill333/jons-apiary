50565a6779596c18743f43af955361d3f66c7c12
Jon Hill <jondhill1984@gmail.com>
2021-03-15 12:21:14 +0200

    Update to conditional render last news API section only if enough articles

--- components/newsDisplay/newsDisplay.js
+++ components/newsDisplay/newsDisplay.js
@@ -1,31 +1,28 @@
 import NewsApiArticles from "../newsApiArticles/newsApiArticles";
 import GAndNytArticles from "../gAndNytArticles/gAndNytArticles";
+import { useEffect } from "react";
 
-export default function NewsDisplay({ articles }) {
+export default function NewsDisplay({ gAndNytArticles, newsApiArticles }) {
   let newsStoreOne;
   let newsStoreTwo;
   let newsStoreThree;
   let newsStoreFour;
-  let gAndNytStoreMain;
   let gAndNytStoreOne;
   let gAndNytStoreTwo;
   let gAndNytStorethree;
   let gAndNytStoreFour;
 
-  if (articles) {
-    newsStoreOne = articles.slice(0, 5);
-    newsStoreTwo = articles.slice(5, 10);
-    newsStoreThree = articles.slice(10, 15);
-    newsStoreFour = articles.slice(15, 20);
-
-    gAndNytStoreMain = articles.slice(20, 39).sort(() => {
-      return 0.5 - Math.random();
-    });
-
-    gAndNytStoreOne = gAndNytStoreMain.slice(0, 4);
-    gAndNytStoreTwo = gAndNytStoreMain.slice(4, 8);
-    gAndNytStorethree = gAndNytStoreMain.slice(8, 12);
-    gAndNytStoreFour = gAndNytStoreMain.slice(12, 16);
+  if (gAndNytArticles) {
+    gAndNytStoreOne = gAndNytArticles.slice(0, 4);
+    gAndNytStoreTwo = gAndNytArticles.slice(4, 8);
+    gAndNytStorethree = gAndNytArticles.slice(8, 12);
+    gAndNytStoreFour = gAndNytArticles.slice(12, 16);
+  }
+  if (newsApiArticles) {
+    newsStoreOne = newsApiArticles.slice(0, 5);
+    newsStoreTwo = newsApiArticles.slice(5, 10);
+    newsStoreThree = newsApiArticles.slice(10, 15);
+    newsStoreFour = newsApiArticles.slice(15, 20);
   }
 
   return (
@@ -36,7 +33,7 @@ export default function NewsDisplay({ articles }) {
       <GAndNytArticles data={gAndNytStoreTwo} />
       <NewsApiArticles data={newsStoreThree} />
       <GAndNytArticles data={gAndNytStorethree} />
-      <NewsApiArticles data={newsStoreFour} />
+      {newsApiArticles.length > 14 && <NewsApiArticles data={newsStoreFour} />}
       <GAndNytArticles data={gAndNytStoreFour} />
     </>
   );
